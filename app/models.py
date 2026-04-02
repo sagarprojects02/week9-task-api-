137a9e6fd1b710d8fc7adcb213cb2f1b4198a5b6
sagarprojects02 <saisagaryadav143@gmail.com>
2026-04-02 22:47:56 +0530

    Add User and Task models to the application

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app.extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    status = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
