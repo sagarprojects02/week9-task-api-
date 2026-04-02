27cdebf02deeced417562bbc0c5ecb12a921396c
sagarprojects02 <saisagaryadav143@gmail.com>
2026-04-02 22:58:15 +0530

    Add JSON requirement decorator for Flask routes

---
+++ utils/decorators.py
@@ -0,0 +1,10 @@
+from functools import wraps
+from flask import request, jsonify
+
+def require_json(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not request.is_json:
+            return jsonify({"error": "JSON required"}), 400
+        return f(*args, **kwargs)
+    return wrapper
